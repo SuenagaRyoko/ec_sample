d2b20ef63a74991e78dac1de9dea3b65940ad300
堀田和尭 <kazutaka.hotta@breezegroup.jp>
2019-07-21 13:10:41 +0900

    ハンズオン解説用

--- js/confirm.js
+++ js/confirm.js
@@ -1,3 +1,33 @@
 window.onload = function () {
+  'use strict';
+  const items = JSON.parse(localStorage.getItem("items")),//ローカルストレージの商品データの配列
+  ele = document.getElementById('js_shopping_list'),//カートの商品を追加する要素
+  fragment = document.createDocumentFragment(),//DOMの追加処理用のフラグメント
+  total_ele = document.getElementById('js_total'),//商品の合計金額表示用の要素
+  confirm_btn = document.getElementById('js_confirm');//購入確定ボタン
+
+  let total = 0;//商品の合計金額
+
+  if (items) {
+    // カート商品の数分、要素を作成
+    /*
+      <li>
+        <h2>{{ 商品名 }}</h2>
+        <div class="price">{{ 金額 }}</div>
+      </li>
+    */
+
+  }
+
+  // 作成した要素の追加
+  ele.appendChild(fragment);
+  // 合計金額の表示
+  total_ele.innerHTML = format_cs(total);
+
+  confirm_btn.addEventListener('click',function () {
+    window.alert(`購入が確定しました。\xA5${ format_cs(total) }`);
+    //localStorageを削除
+  });
+
 
 };
--- js/script.js
+++ js/script.js
@@ -1,4 +1,85 @@
 window.onload = function () {
+  'use strict';
+
+  const cart_btns = document.querySelectorAll('.js_cart_btn'),//カートボタン
+  cart_cnt_icon = document.getElementById('js_cart_cnt'),//カートの個数アイコン
+  clicked = [],//クリックされたカートアイコンのインデックス
+  save_items = [],//ローカルストレージ保存用の配列
+  items = JSON.parse(localStorage.getItem("items"));//ローカルストレージの商品データ配列
+  let cart_cnt = 0;//カートのアイテム数
+  // すでにカートに商品が入っている場合、カートアイコンのカウント表示とカートボタンをアクティブにする
+  if (items) {
+    let id;
+    for (let i = 0; i < items.length; i++) {
+      id = items[i].id;
+      save_items.push(items[i]);
+      clicked.push(id);
+      activate_btn(id);
+    }
+
+    if(items.length != 0){
+      cart_cnt_icon.parentNode.classList.remove('hidden');
+      cart_cnt_icon.innerHTML = cart_cnt;
+    }
+  }
+
+  // カートボタンを押した際の処理
+  cart_btns.forEach(function (cart_btn, index) {
+    cart_btn.addEventListener('click', function () {
+
+      if (/* カートボタンがすでに押されているかの判定 */) {
+
+        for (let i = 0; i < clicked.length; i++) {
+          if(clicked[i] == index){
+            clicked.splice(i, 1);
+            save_items.splice(i, 1);
+          }
+        }
+
+        inactivate_btn(index);
+
+      }else if(/* カートボタンが押されていない場合 */){
+
+
+        let name,//商品の名前を取得
+        price;//商品の値段を取得
+
+        //クリックされた商品をインデックス(clicked)に追加
+
+        //ローカルストレージ保存用の配列に追加
+        /*
+        * @param {Number} id:    商品の識別番号
+        * @param {String} name:  商品の名前
+        * @maram {Number} price: 商品の値段
+        */
+
+        activate_btn(index);
+
+      }
+
+      // ローカルストレージに商品データを保管
+
+    });
+  });
+
+  function activate_btn(index) {
+    cart_cnt++;
+    if( cart_cnt >= 1 ){
+      cart_cnt_icon.parentNode.classList.remove('hidden');
+    }
+    cart_cnt_icon.innerHTML = cart_cnt;
+    cart_btns[index].classList.add('item_cart_btn_active');
+  }
+
+  function inactivate_btn(index) {
+    cart_cnt--;
+    if(cart_cnt == 0){
+      cart_cnt_icon.parentNode.classList.add('hidden');
+    }
+    cart_cnt_icon.innerHTML = cart_cnt;
+    cart_btns[index].classList.remove('item_cart_btn_active');
+  }
+
 
 };
 
